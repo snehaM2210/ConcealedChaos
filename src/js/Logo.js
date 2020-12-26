408af2d495134eac882cbb89bb04cba1f6b014fb
Anonymous-coder-its-me <dsdarimisetti@gmail.com>
2020-12-26 23:59:21 +0530

    firebase authenticatiion

--- src/js/Logo.js
+++ src/js/Logo.js
@@ -2,13 +2,12 @@ import React from "react";
 import "../css/Logo.css";
 
 function Logo() {
-  return (
-    <img
-      className="logo"
-      src=""
-      alt=""
-    />
-  );
+    return ( <
+        img className = "logo"
+        src = ""
+        alt = "LOGO" /
+        >
+    );
 }
 
 export default Logo;
