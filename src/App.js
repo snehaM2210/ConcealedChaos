00f40a22d2421b685e3b195f6465fbacce4721ee
Anonymous-coder-its-me <dsdarimisetti@gmail.com>
2020-12-29 14:37:43 +0530

    add delete option for posts and post anonymously

--- src/App.js
+++ src/App.js
@@ -1,32 +1,33 @@
-import React, { useState } from "react";
-import Header from "./js/Header";
-import Posts from "./js/Posts";
-import ImageUpload from "./js/ImageUpload";
-import Button from "@material-ui/core/Button";
-import "./css/App.css";
-import "./css/elements.css";
-import { Route, Switch, useHistory, useLocation } from "react-router-dom";
-import MyPosts from "./js/MyPosts";
+import React, { useState } from 'react';
+import Header from './js/Header';
+import Posts from './js/Posts';
+import ImageUpload from './js/ImageUpload';
+import Button from '@material-ui/core/Button';
+import './css/App.css';
+import './css/elements.css';
+import { Route, Switch, useHistory, useLocation } from 'react-router-dom';
+import MyPosts from './js/MyPosts';
+import Profile from './js/Profile';
 
 function App() {
   const [user, setUser] = useState(null);
   const history = useHistory();
   const location = useLocation();
-  const paths = ["/"];
-  console.log("location is ", location);
+  const paths = ['/'];
+  console.log('location is ', location);
   return (
-    <div className="app">
+    <div className='app'>
       <Header user={user} setUser={setUser} />
 
       {!paths.includes(location.pathname) && (
         <div
-          style={{ cursor: "pointer", margin: "1rem" }}
+          style={{ cursor: 'pointer', margin: '1rem' }}
           onClick={() => {
-            console.log("clicked");
+            console.log('clicked');
             history.goBack();
           }}
         >
-          <Button variant="outlined" color="secondary">
+          <Button variant='outlined' color='secondary'>
             go back
           </Button>
         </div>
@@ -35,11 +36,11 @@ function App() {
       <Switch>
         <Route
           exact
-          path="/"
+          path='/'
           render={() => (
             <>
               {user && user?.displayName && (
-                <ImageUpload username={user?.displayName} />
+                <ImageUpload username={user?.displayName} email={user?.email} />
               )}
               <Posts user={user} />
             </>
@@ -47,8 +48,13 @@ function App() {
         />
         <Route
           exact
-          path="/myposts"
+          path='/myposts'
           render={(routeProps) => <MyPosts {...routeProps} user={user} />}
+        />
+        <Route
+          exact
+          path='/myprofile'
+          render={(routeProps) => <Profile {...routeProps} user={user} />}
         />
       </Switch>
     </div>
--- src/js/ImageUpload.js
+++ src/js/ImageUpload.js
@@ -1,6 +1,6 @@
-import React, { useState } from "react";
-import firebase from "firebase";
-import { db, storage } from "./firebase";
+import React, { useState } from 'react';
+import firebase from 'firebase';
+import { db, storage } from './firebase';
 import {
   Modal,
   Button,
@@ -8,59 +8,70 @@ import {
   LinearProgress,
   IconButton,
   makeStyles,
-} from "@material-ui/core";
-import Chip from "@material-ui/core/Chip";
-import InputLabel from "@material-ui/core/InputLabel";
-import MenuItem from "@material-ui/core/MenuItem";
-import FormControl from "@material-ui/core/FormControl";
-import Select from "@material-ui/core/Select";
+  FormControlLabel,
+} from '@material-ui/core';
+import Chip from '@material-ui/core/Chip';
+import InputLabel from '@material-ui/core/InputLabel';
+import MenuItem from '@material-ui/core/MenuItem';
+import FormControl from '@material-ui/core/FormControl';
+import Select from '@material-ui/core/Select';
 
-import "../css/ImageUpload.css";
+import '../css/ImageUpload.css';
+import Switch from '@material-ui/core/Switch';
 
 const allFailTypes = [
-  "betrayal",
-  "health issues",
-  "financial crisis",
-  "poor performance",
-  "boredom",
-  "confusion ",
-  "unfair treatment",
-  "career pressure",
-  "uncracked interview",
-  "uncracked entrance exam",
-  "no passion",
-  "failed relations",
-  "zero knowledge",
+  'betrayal',
+  'health issues',
+  'financial crisis',
+  'poor performance',
+  'boredom',
+  'confusion ',
+  'unfair treatment',
+  'career pressure',
+  'uncracked interview',
+  'uncracked entrance exam',
+  'no passion',
+  'failed relations',
+  'zero knowledge',
 ];
 
 const useStyles = makeStyles((theme) => ({
   formControl: {
     margin: theme.spacing(1),
-    minWidth: "100%",
+    minWidth: '100%',
   },
   selectEmpty: {
     marginTop: theme.spacing(2),
   },
   chips__container: {
-    display: "flex",
-    alignItems: "center",
-    flexWrap: "wrap",
-    "& > *": {
+    display: 'flex',
+    alignItems: 'center',
+    flexWrap: 'wrap',
+    '& > *': {
       margin: theme.spacing(0.5),
     },
   },
   input: {
-    marginBottom: "1rem",
+    marginBottom: '1rem',
+  },
+  switch: {
+    marginRight: 'auto',
   },
 }));
 
-function ImageUpload({ username }) {
+function ImageUpload({ username, email }) {
+  const classes = useStyles();
   const [open, setOpen] = useState(false);
-  const [image, setImage] = useState("");
+  const [image, setImage] = useState('');
   const [progress, setProgress] = useState(0);
-  const [title, setTitle] = useState("");
-  const [caption, setCaption] = useState("");
-  const [fail, setFail] = React.useState([]);
+  const [title, setTitle] = useState('');
+  const [caption, setCaption] = useState('');
+  const [fail, setFail] = useState([]);
+  const [checked, setChecked] = useState({
+    checkedA: false,
+  });
+
+  console.log(checked);
 
   const handleChange = (e) => {
     setFail((fail) => [...fail, e.target.value]);
@@ -69,17 +80,22 @@ function ImageUpload({ username }) {
   const handleFileChange = (e) => {
     if (e.target?.files[0]) setImage(e.target.files[0]);
   };
+
   const handleFileUpload = (e) => {
     e.preventDefault();
     if (!image) {
-      db.collection("posts").add({
+      db.collection('posts').add({
         timestamp: firebase.firestore.FieldValue.serverTimestamp(),
         title,
         caption,
-        userName: username,
+        userName: checked.checkedA ? 'Anonymous' : username,
         failTags: fail,
+        userEmail: email,
       });
-      setCaption("");
+      setTitle('');
+      setCaption('');
+      setFail([]);
+      setChecked({ checkedA: false });
       handleClose();
       return;
     }
@@ -87,7 +103,7 @@ function ImageUpload({ username }) {
     const uploadTask = storage.ref(`images/${image.name}`).put(image);
 
     uploadTask.on(
-      "state_changed",
+      'state_changed',
       (snapshot) => {
         // progress function
         const progress = Math.round(
@@ -103,76 +119,83 @@ function ImageUpload({ username }) {
       () => {
         // complete function
         storage
-          .ref("images")
+          .ref('images')
           .child(image.name)
           .getDownloadURL()
           .then((url) => {
-            db.collection("posts").add({
+            db.collection('posts').add({
               timestamp: firebase.firestore.FieldValue.serverTimestamp(),
               title,
               caption,
               imageURL: url,
               userName: username,
+              userEmail: email,
               failTags: fail,
             });
 
             setImage(null);
             setProgress(true);
-            setCaption("");
+            setCaption('');
             setFail([]);
             handleClose();
           });
       }
     );
   };
+
   const handleClose = () => {
     setOpen(false);
     setProgress(null);
   };
+
   const handleChipDelete = (toBeDeletedFail) => {
     setFail((fail) => fail.filter((eachFail) => eachFail !== toBeDeletedFail));
   };
 
-  const classes = useStyles();
-  console.log(fail);
+  const handleChangeAnonymous = (event) => {
+    setChecked({ ...checked, [event.target.name]: event.target.checked });
+  };
+
+  console.log();
+
   return (
-    <div className="imageUpload">
+    <div className='imageUpload'>
       <div
-        className="writePost__container"
+        className='writePost__container'
         onClick={() => {
           setOpen(true);
         }}
       >
-        <div className="writePost__main">
+        <div className='writePost__main'>
           <IconButton>
-            <i className="fas fa-edit"></i>
+            <i className='fas fa-edit'></i>
           </IconButton>
           <p>write a post</p>
         </div>
       </div>
-      <Modal className="modal" open={open} onClose={handleClose}>
+      <Modal className='modal' open={open} onClose={handleClose}>
         <div>
-          <form className="modal__body" onSubmit={handleFileUpload}>
-            <h2 style={{ marginRight: "auto", marginBottom: "1rem" }}>
+          <form className='modal__body' onSubmit={handleFileUpload}>
+            <h2 style={{ marginRight: 'auto', marginBottom: '1rem' }}>
               Create A Post
             </h2>
             <TextField
               className={classes.input}
               required
-              type="text"
-              id="title"
+              type='text'
+              id='title'
               fullWidth
-              label="Title of your fail"
+              label='Title of your fail'
               value={title}
               onChange={(e) => setTitle(e.target.value)}
             />
             <TextField
               className={classes.input}
               required
-              type="text"
-              id="capiton"
+              type='text'
+              id='capiton'
               fullWidth
-              label="Enter the caption..."
+              label='Enter the caption...'
               value={caption}
               onChange={(e) => setCaption(e.target.value)}
             />
@@ -181,20 +204,20 @@ function ImageUpload({ username }) {
                 fail.map((eachFail) => (
                   <Chip
                     key={`${eachFail}-${Math.round(Math.random() * 999999)}`}
-                    color="primary"
+                    color='primary'
                     label={eachFail}
                     onDelete={() => handleChipDelete(eachFail)}
                   />
                 ))}
             </div>
             <FormControl className={classes.formControl}>
-              <InputLabel id="demo-simple-select-label">
+              <InputLabel id='demo-simple-select-label'>
                 Potential reasons / relevant tags for failure
               </InputLabel>
               <Select
-                labelId="demo-simple-select-label"
-                id="demo-simple-select"
-                value={""}
+                labelId='demo-simple-select-label'
+                id='demo-simple-select'
+                value={''}
                 onChange={handleChange}
               >
                 {allFailTypes.map((eachFail) => (
@@ -207,24 +230,37 @@ function ImageUpload({ username }) {
                 ))}
               </Select>
             </FormControl>
-            <div className="modal__formButtons">
-              <Button component="label" variant="outlined" color="secondary">
+            <FormControlLabel
+              className={classes.switch}
+              control={
+                <Switch
+                  checked={checked.checkedA}
+                  onChange={handleChangeAnonymous}
+                  name='checkedA'
+                  inputProps={{ 'aria-label': 'secondary checkbox' }}
+                />
+              }
+              label='Post anonymously'
+            />
+
+            <div className='modal__formButtons'>
+              <Button component='label' variant='outlined' color='secondary'>
                 Choose File *
                 <input
-                  type="file"
-                  name="file"
-                  id="file"
-                  accept="image/*"
-                  style={{ display: "none" }}
+                  type='file'
+                  name='file'
+                  id='file'
+                  accept='image/*'
+                  style={{ display: 'none' }}
                   onChange={handleFileChange}
                 />
               </Button>
-              <Button type="submit" variant="contained" color="primary">
+              <Button type='submit' variant='contained' color='primary'>
                 submit
               </Button>
             </div>
             {image ? (
-              <div className="imageUpload__text">
+              <div className='imageUpload__text'>
                 <span>
                   file choosen <i>{image?.name}</i>
                 </span>
@@ -232,13 +268,13 @@ function ImageUpload({ username }) {
             ) : null}
             {progress > 0 && (
               <div
-                style={{ width: "100%", display: "block", marginTop: "2rem" }}
+                style={{ width: '100%', display: 'block', marginTop: '2rem' }}
               >
                 <LinearProgress
-                  variant="determinate"
+                  variant='determinate'
                   value={50}
-                  className="imageUpload__progress"
-                  max="100"
+                  className='imageUpload__progress'
+                  max='100'
                 />
               </div>
             )}
